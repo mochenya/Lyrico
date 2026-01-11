0d2ef1ef9302b14695e45eb1a644b645f09481c2
Replica0110 <125732107+Replica0110@users.noreply.github.com>
2026-01-11 19:37:27 +0800

    chore(app): 更新应用版本号

--- lyrico-app/build.gradle.kts
+++ lyrico-app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         minSdk = 28
         //noinspection ExpiredTargetSdkVersion
         targetSdk = 28
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
